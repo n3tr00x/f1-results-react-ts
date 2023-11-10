729c240c0b6187425327b23dae52b0f9bc5eec3b
Hubert Warchoł <n3tr00x99@gmail.com>
2023-11-10 03:31:25 +0100

    Add 3 types Montserrat font locally
    Montserrat font types:
    - regular
    - italic
    - bold
    
    Append new fonts in index.css
    Appropriate configuration in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		fontFamily: {
+			'sans': ['montserrat-regular', 'sans-serif'],
+		},
+		extend: {
+			fontFamily: {
+				'mt-bold': ['montserrat-bold', 'sans-serif'],
+				'mt-italic': ['montserrat-italic', 'sans-serif'],
+			},
+		},
 	},
 	plugins: [],
 };
