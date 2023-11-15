1447ad4903a771117c12ada1cfde943940f21a4e
Hubert Warchoł <n3tr00x99@gmail.com>
2023-11-16 00:29:01 +0100

    style: Add animation for modals (fade-slide-up)

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,21 @@ export default {
 				'mt-bold': ['montserrat-bold', 'sans-serif'],
 				'mt-italic': ['montserrat-italic', 'sans-serif'],
 			},
+			animation: {
+				'slide-up': 'fade-slide-up 0.3s ease-out forwards',
+			},
+			keyframes: {
+				'fade-slide-up': {
+					'from': {
+						opacity: 0,
+						transform: 'translateY(4rem)',
+					},
+					'to': {
+						opacity: 1,
+						transform: 'translateY(0)',
+					},
+				},
+			},
 		},
 	},
 	plugins: [],
