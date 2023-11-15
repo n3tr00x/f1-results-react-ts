7224d1ee209c08e6a542f70457a9165bb84a0379
Hubert Warchoł <n3tr00x99@gmail.com>
2023-11-15 02:16:13 +0100

    refactor: Add wrapper for Header component
    The header wrapper is limited to 1200 px

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,13 @@ import { Navigation } from './Navigation';
 
 export function Header() {
 	return (
-		<header className="bg-gradient-to-br from-red-400 to-red-600 p-4 shadow-xl md:flex md:justify-between">
-			<h1 className="md:b text-center font-mt-bold text-2xl uppercase text-slate-50 md:self-center">
-				f1 results
-			</h1>
-			<Navigation />
+		<header className="bg-gradient-to-br from-red-400 to-red-600 p-4 shadow-xl">
+			<div className="mx-auto my-0 w-full max-w-[1200px] md:flex md:justify-between">
+				<h1 className="md:b text-center font-mt-bold text-2xl uppercase text-slate-50 md:self-center">
+					f1 results
+				</h1>
+				<Navigation />
+			</div>
 		</header>
 	);
 }
