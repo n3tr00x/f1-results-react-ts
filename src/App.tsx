36a12c23c0ca8fe26cbb7b34e625429fe2e365da
Hubert Warchoł <n3tr00x99@gmail.com>
2023-11-15 01:49:11 +0100

    Add ModalContextProvider and Header to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import { ModalContextProvider } from './contexts/ModalContext';
+import { Header } from './components/Header';
+
 export function App() {
-	return <h1>Hello world!</h1>;
+	return (
+		<ModalContextProvider>
+			<Header />
+		</ModalContextProvider>
+	);
 }
