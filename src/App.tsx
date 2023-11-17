439d9bf393994d3e90f7af1f49efdcfec5195cb9
Hubert Warchoł <n3tr00x99@gmail.com>
2023-11-17 02:38:40 +0100

    refactor: Change of location modal provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import { ModalContextProvider } from './contexts/ModalContext';
 import { Header } from './components/Header';
 import { SeasonPickerModal } from './components/modals/SeasonPicker';
 
 export function App() {
 	return (
-		<ModalContextProvider>
+		<>
 			<Header />
 			<SeasonPickerModal />
-		</ModalContextProvider>
+		</>
 	);
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 
 import { App } from './App.tsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ModalContextProvider } from './contexts/ModalContext.tsx';
 import './index.css';
 
 const queryClient = new QueryClient();
@@ -10,7 +11,9 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<QueryClientProvider client={queryClient}>
-			<App />
+			<ModalContextProvider>
+				<App />
+			</ModalContextProvider>
 		</QueryClientProvider>
 	</StrictMode>,
 );
