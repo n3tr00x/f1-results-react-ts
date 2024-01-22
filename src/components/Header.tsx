0ab60c2f364a9bae3d41db36409d91ea7d8d6789
Hubert Warchoł <n3tr00x99@gmail.com>
2024-01-22 14:27:40 +0100

    style: Implement styles for desktop

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,9 @@ import { Navigation } from './Navigation';
 
 export function Header() {
 	return (
-		<header className="bg-gradient-to-br from-red-400 to-red-600 p-4 shadow-xl">
-			<div className="mx-auto my-0 w-full max-w-[1200px] md:flex md:justify-between">
-				<h1 className="md:b text-center font-mt-bold text-2xl uppercase text-slate-50 md:self-center">
+		<header className="bg-gradient-to-br from-red-400 to-red-600 p-4 shadow-xl lg:min-h-screen lg:w-[320px] lg:min-w-[240px]">
+			<div className="mx-auto my-0 w-full">
+				<h1 className="text-center font-f1-bold text-2xl uppercase text-slate-50 md:self-center">
 					f1 results
 				</h1>
 				<Navigation />
