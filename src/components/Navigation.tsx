81b76fde30a538d87f17b16292a483753133d49c
Hubert Warchoł <n3tr00x99@gmail.com>
2024-01-22 02:04:08 +0100

    style: Add menu item for standings and button handlers
    adjusted menu to desktop width

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,21 +1,32 @@
 import { useContext } from 'react';
 import { Button } from './UI/Button';
 import { ModalContext } from '../contexts/ModalContext';
+import { F1SeasonContext } from '../contexts/F1SeasonContext';
 
 export function Navigation() {
-	const { openSeasonsModal } = useContext(ModalContext);
+	const { openSeasonsModal, openCalendarModal, openStandingsModal } =
+		useContext(ModalContext);
+	const { updateRound, updateSeason } = useContext(F1SeasonContext);
+
+	const handleLatestRaceClick = () => {
+		updateSeason('current');
+		updateRound('last');
+	};
 
 	return (
-		<nav className="fixed inset-x-0 bottom-0 bg-gradient-to-br from-red-400 to-red-600 p-4 shadow-xl md:static md:bg-transparent md:from-transparent md:to-transparent md:p-0 md:shadow-none">
-			<ul className="flex justify-center gap-4 text-sm text-slate-50">
+		<nav className="fixed inset-x-0 bottom-0 bg-gradient-to-br from-red-400 to-red-600 p-4 lg:static lg:bg-transparent lg:from-transparent lg:to-transparent">
+			<ul className="flex justify-center gap-2 text-xs text-slate-50 lg:flex-col lg:text-sm">
 				<li>
-					<Button onClick={openSeasonsModal}>Sezon</Button>
+					<Button onClick={openSeasonsModal}>Season</Button>
 				</li>
 				<li>
-					<Button>Ostatni wyścig</Button>
+					<Button onClick={handleLatestRaceClick}>Latest race</Button>
 				</li>
 				<li>
-					<Button>Grand Prix</Button>
+					<Button onClick={openCalendarModal}>Schedule</Button>
+				</li>
+				<li>
+					<Button onClick={openStandingsModal}>Standings</Button>
 				</li>
 			</ul>
 		</nav>
